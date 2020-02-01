f2eba3789c16960b01623428519a31e8587c6b00
callanbr <chrisallan1992@icloud.com>
2020-01-31 20:57:15 -0700

    generate random group name

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, {useState} from 'react';
-import Bundle from "./components/ExampleWithProps/Bundle";
+
+import nouns from "./components/nouns";
+import adjectives from './components/adjectives';
+
+
+const generateHackerName = () => {
+    const adjective  = adjectives[Math.floor(Math.random() * adjectives.length)];
+    const noun       = nouns[Math.floor(Math.random() * nouns.length)];
+    const hackerName = (`${adjective} ${noun}`);
+    return hackerName;
+}
+
 
 function App(props) {
 
 
     return (
-      <>
-        <Bundle/>
-      </>
+        <div className="App">
+            Your new group name is... <br/>
+            <h2>{generateHackerName()}</h2>
+                <p>
+                    <a href="https://github.com/callanbr/ProjectNameGen">GitHub</a>
+                </p>
+        </div>
     );
   }
   
