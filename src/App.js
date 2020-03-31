e88dac3bef386a2cc1f8dbc34f74ce5d265c60ce
callanbr <chrisallan1992@icloud.com>
2020-03-31 14:32:00 -0700

    generate name

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import nouns from "./components/nouns";
 import adjectives from './components/adjectives';
 
 
-const generateHackerName = () => {
+const generateName = () => {
     const adjective  = adjectives[Math.floor(Math.random() * adjectives.length)];
     const noun       = nouns[Math.floor(Math.random() * nouns.length)];
     const groupName = (`${adjective} ${noun}`);
@@ -18,9 +18,9 @@ function App(props) {
     return (
         <div className="App">
             Your new group name is... <br/>
-            <h2>{generateHackerName()}</h2>
+            <h2>{generateName()}</h2>
                 <p>
-                    <a href="https://github.com/callanbr/ProjectNameGen">GitHub</a>
+                    <a href="https://github.com/callanbr/project-name-gen">GitHub</a>
                 </p>
         </div>
     );
